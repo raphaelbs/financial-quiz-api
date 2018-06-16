c18ae7c7ff4b9d9d440064df2b26d5e5978caea9
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-16 19:26:49 -0300

    ajustes na estrutura

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app/app.controller';
+import { AppService } from './app/app.service';
 import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_CONNECTOR || 'mongodb://localhost/financialquiz')],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGO_CONNECTOR || 'mongodb://localhost/financialquiz',
+    ),
+  ],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
