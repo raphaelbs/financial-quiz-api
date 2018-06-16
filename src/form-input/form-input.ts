5fdb53ca1f507a314c2e3740615806d4076f985c
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-16 20:29:21 -0300

    form-input incluido

---
+++ src/form-input/form-input.ts
@@ -0,0 +1,27 @@
+import * as mongoose from 'mongoose';
+
+export const FormInput = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  key: {
+    type: String,
+    required: true,
+  },
+  required: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  children: [
+    {
+      type: this,
+      required: false,
+    },
+  ],
+});
