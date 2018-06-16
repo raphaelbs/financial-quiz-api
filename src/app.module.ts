c09e44198234d6e07f3ff8c5d6cae3a7a6d87727
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-16 19:06:32 -0300

    mongoose instalado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGO_CONNECTOR || 'mongodb://localhost/financialquiz')],
   controllers: [AppController],
   providers: [AppService],
 })
