9883a40be3230c251672510481b8c45dd6fb5a76
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-21 21:47:17 -0300

    cors habilitado para localhost:4200 e aws s3

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { Environment } from './enviroment';
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:4200',
+        'http://financial-quiz-app.s3-website-us-east-1.amazonaws.com/',
+      ],
+    }),
+  );
   await app.listen(Environment.PORT);
 }
 bootstrap();
